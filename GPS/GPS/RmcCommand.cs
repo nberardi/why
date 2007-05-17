b5e25eaf5d018e72d8128065871472fe5bf1d2cc
nberardi <nberardi@2ea35cf0-9819-0410-8aa1-e5eaea0cf295>
2007-05-17 01:16:59 +0000

    GPS Client

---
+++ GPS/GPS/RmcCommand.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GPS
+{
+	/// <summary>
+	/// Recommended Minimum sentence C
+	/// </summary>
+	/// <seealso href="http://www.gpsinformation.org/dale/nmea.htm#RMC"/>
+	public struct RmcCommand
+	{
+		private DateTime _takenAt;
+		private char _status;
+		private Location _location;
+		private float _speedKnots;
+		private float? _trackAngle;
+		private decimal _magneticVariation;
+
+		public RmcCommand (string command)
+		{
+			string[] data = command.Split(new char[] { ',' }, StringSplitOptions.None);
+
+			// format in HHmmss
+			_takenAt = new DateTime(
+				2000 + Convert.ToInt32(data[9].Substring(4, 2)),
+				Convert.ToInt32(data[9].Substring(2, 2)),
+				Convert.ToInt32(data[9].Substring(0, 2)),
+				Convert.ToInt32(data[1].Substring(0, 2)),
+				Convert.ToInt32(data[1].Substring(2, 2)),
+				Convert.ToInt32(data[1].Substring(4, 2)),
+				DateTimeKind.Utc
+			);
+
+			_status = data[2][0];
+
+			int latitudeDeg = Convert.ToInt32(data[3].Substring(0, 2));
+			decimal latitudeMins = Convert.ToDecimal(data[3].Substring(2));
+
+			decimal latitude = (((decimal)latitudeDeg) + (latitudeMins / 60M)) * (data[4].ToUpper() == "N" ? 1M : -1M);
+
+			int longitudeDeg = Convert.ToInt32(data[5].Substring(0, 3));
+			decimal longitudeMins = Convert.ToDecimal(data[5].Substring(3));
+
+			decimal longitude = (((decimal)longitudeDeg) + (longitudeMins / 60M)) * (data[6].ToUpper() == "E" ? 1M : -1M);
+
+			_location = new Location(latitude, longitude, 0F);
+
+			_speedKnots = Convert.ToSingle(data[7]);
+
+			_trackAngle = String.IsNullOrEmpty(data[8]) ? (float?)null : Convert.ToSingle(data[8]);
+
+			_magneticVariation = Convert.ToDecimal(data[9]) * (data[10].ToUpper() == "E" ? 1M : -1M);
+		}
+	}
+}
